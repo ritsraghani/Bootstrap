0d7f8bed5cf4957f1a0d3968d35714e4a23172a3
Ritu Raghani <rits.raghani@gmail.com>
2017-12-09 14:31:39 +0530

    insert page

---
+++ insert.php
@@ -0,0 +1,29 @@
+<?php 
+
+$data=json_decode(file_get_contents("php://input"));
+
+echo $emname=$data->name;
+echo $emdob=$data->dob;
+echo $ememail=$data->emailid;
+echo $emmobile = $data->mobile;
+echo $emsalary = $data->salary;
+echo $emcity = $data->city;
+
+$con = mysqli_connect ('localhost','root','');
+mysqli_select_db($con,'angularjs');
+
+echo $sql= "insert into employee (employee_name,dob,email,mobile_no,salary,city) 
+values('$emname','$emdob','$ememail','$emmobile','$emsalary','$emcity')";
+
+$result=mysqli_query($con,$sql) or die(mysqli_error());
+
+
+// while($row=mysqli_fetch_assoc($result))
+// {
+//     $myJSON[]=$row;
+// }
+
+
+// $a=json_encode($myJSON);
+// print_r($a);
+?>
